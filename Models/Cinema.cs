79f090af6e15761ece251fb2066e126d92ab24a8
Surgeous <tuanbao2311@gmail.com>
2023-03-03 07:48:18 +0700

    Tạo các Cinema Models

---
+++ Models/Cinema.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public string CinemaID { get; set; }
+        public string CinemaLogo { get; set; }
+        public string CinemaName { get; set; }
+        public string CinemaDescription { get; set; }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using eTickets.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public string MovieID { get; set; }
+        public string MovieName { get; set; }
+        public string MovieDescription { get; set; }
+        public double MoviePrice { get; set; }
+        public string MovieImageURL { get; set; }
+        public DateTime MovieStartDate { get; set; }
+        public DateTime MovieEndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
